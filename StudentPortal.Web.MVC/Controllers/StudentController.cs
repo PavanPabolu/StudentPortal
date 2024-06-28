6cc3662dfa25b04e7db6aa002df7ecf7a339c7cd
PavanPabolu <PavanPabolu.edu@gmail.com>
2024-06-28 20:16:31 +0530

    CRUD operations with basic form submission

--- StudentPortal.Web.MVC/Controllers/StudentController.cs
+++ StudentPortal.Web.MVC/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StudentPortal.Web.MVC.Data;
 using StudentPortal.Web.MVC.Models;
 using StudentPortal.Web.MVC.Models.Entities;
@@ -35,6 +36,56 @@ namespace StudentPortal.Web.MVC.Controllers
             await _dbContext.SaveChangesAsync();
 
             return View();
+        }
+
+        public async Task<IActionResult> List()
+        {
+            var students = await _dbContext.Students.ToListAsync();
+
+            return View(students);
+            //return Ok(students);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var student = await _dbContext.Students.FindAsync(id);
+
+            return View(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student entity)
+        {
+            var student = await _dbContext.Students.FindAsync(entity.Id);
+
+            if (student is not null)
+            {
+                student.Name = entity.Name;
+                student.Email = entity.Email;
+                student.Phone = entity.Phone;
+                student.Subscribed = entity.Subscribed;
+            }
+            await _dbContext.SaveChangesAsync();
+
+            return RedirectToAction("List", "Student");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Student entity)//(Guid id)
+        {
+            //Occurs Error "The instance of entity type 'Student' cannot be tracked because another instance with the same key value for {id} is already being tracked."
+            //var student = await _dbContext.Students.FindAsync(entity.Id); 
+            var student = await _dbContext.Students
+                            .AsNoTracking()
+                            .FirstOrDefaultAsync(x => x.Id == entity.Id);
+
+            if (student is not null)
+            {
+                _dbContext.Students.Remove(entity);
+                await _dbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("List", "Student");
         }
     }
 }
